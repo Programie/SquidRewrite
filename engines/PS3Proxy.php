107ea232830962f5079fe1ce99c3a13f8418c543
Michael Wieland <github@selfcoders.com>
2014-12-15 19:18:58 +0100

    Added PS3 Proxy

---
+++ engines/PS3Proxy.php
@@ -0,0 +1,72 @@
+<?php
+class PS3Proxy extends Engine
+{
+	/**
+	 * @var PDO
+	 */
+	private $pdo;
+	/**
+	 * @var string
+	 */
+	private $pkgPath;
+	/**
+	 * @var string
+	 */
+	private $localUrl;
+
+	public function __construct(Pini $config)
+	{
+		parent::__construct($config);
+
+		$this->pdo = new PDO($config->getValue("database", "dsn"), $config->getValue("database", "username"), $config->getValue("database", "password"));
+		$this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+		$this->pdo->query("SET NAMES utf8");
+
+		$this->pkgPath = $config->getValue("paths", "pkg");
+		$this->localUrl = $config->getValue("paths", "localUrl");
+	}
+
+	public function process()
+	{
+		if ($this->method != "GET")
+		{
+			return false;
+		}
+
+		if (!preg_match("|^http://zeus\.dl\.playstation\.net/cdn/(.*)/(.*)/(.*)\.pkg|", $this->url, $matches))
+		{
+			return false;
+		}
+
+		$file = $matches[3] . ".pkg";
+
+		if (file_exists($this->pkgPath . "/" . $file))
+		{
+			$this->url = $this->localUrl . $file;
+		}
+		else
+		{
+			$query = $this->pdo->prepare("SELECT `id` FROM `files` WHERE `url` = :url");
+
+			$query->execute(array
+			(
+				":url" => $this->url
+			));
+
+			if (!$query->rowCount())
+			{
+				$query = $this->pdo->prepare("
+					INSERT INTO `files`
+					SET `url` = :url
+				");
+
+				$query->execute(array
+				(
+					":url" => $this->url
+				));
+			}
+		}
+
+		return true;
+	}
+}
---
+++ engines/PS3Proxy.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `groups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `files` (
+	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`groupId` int(10) unsigned DEFAULT NULL,
+	`url` varchar(255) NOT NULL DEFAULT '',
+	`name` varchar(200) DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	KEY `groupId` (`groupId`),
+	CONSTRAINT `files_ibfk_1` FOREIGN KEY (`groupId`) REFERENCES `groups` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
